ed72a9a440c8a63f85c94b358abaa92f76800749
Chainguard-Shujun-Workstation <shujun@Chainguard-Shujun-Workstations-MacBook-Pro.local>
2023-08-03 07:11:30 -0700

    update repo

--- src/compiler/test_compiler.rs
+++ src/compiler/test_compiler.rs
@@ -4,14 +4,14 @@ use super::*;
 fn compiler(){
     let mut cp = Compiler::new();
     cp.initial();
-    // cp.compile("test/DemoData/Intoto/debian/", MetadataType::INTOTO);
-    // let result = cp.query("validate($f).");
-    // if result.state{
-    //     println!("Found solutions:");
-    //     println!("{:?}", result.results);
-    // }
-    // else{
-    //     println!("No compliant result");
-    // }
+    cp.compile("test/DemoData/Intoto/debian/", MetadataType::INTOTO);
+    let result = cp.query("validate($f).");
+    if result.state{
+        println!("Found solutions:");
+        println!("{:?}", result.results);
+    }
+    else{
+        println!("No compliant result");
+    }
     cp.print_nu("test/Example_Output/Intoto.log");
 }
